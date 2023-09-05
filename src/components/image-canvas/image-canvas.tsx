c0d06bbccc659834aa81e5f679c51a5ff9326d7b
Marcelito <mdpilone@gmail.com>
2023-09-05 15:42:37 -0300

    agregando mejoras

--- src/components/image-canvas/image-canvas.tsx
+++ src/components/image-canvas/image-canvas.tsx
@@ -1,5 +1,6 @@
 import { Component, Prop,State, Element } from '@stencil/core';
 import { ContentDataImg, ContentDataText } from '../type';
+import QRCode from 'qrcode'
 
 @Component({
   tag: 'image-canvas',
@@ -54,23 +55,19 @@ export class ImageCanvas {
   private centerText: string = 'start'
 
   componentWillLoad(){
-    if( this.content[0]['type'] != 'image'){
-      console.error('el primer valor del array tiene que ser de type image')
-      return
-    }
     this.canvas = this.__createCanvas();
     console.info("el canvas es", this.canvas)
     this.el.appendChild(this.canvas)
-    const img = new Image();
+    /*const img = new Image();
     img.addEventListener('load', () =>  {
       this.imgLoading = false
       const ctx = this.canvas.getContext('2d');
       /*var anchoImgBg = 
-      var altoImgBg  = */
+      var altoImgBg  = 
       ctx.drawImage(img, 0, 0,this.content[0]['width'],this.content[0]['height']);
-      this.__loadData() 
     })
-    img.src = this.content[0]['any_string'];
+    img.src = this.content[0]['any_string'];*/
+    this.__loadData() 
   }
 
 
@@ -81,25 +78,40 @@ export class ImageCanvas {
     return canvas
   }
 
-  __loadData() {
+
+   __loadData() {
        const ctx = this.canvas.getContext('2d');
        let data = this.content;
-       data.shift()
+
        data.forEach(item => {
-         if( item.type ==='text' ){
+         if( item.type === 'text' ){
             const iText = item as ContentDataText
             ctx.fillStyle = iText.canvasOptions.fillStyle || this.canvasFillStyle;
             ctx.font = iText.canvasOptions.font || this.fontCanvas;
             ctx.textAlign = iText.canvasOptions.textAlign as CanvasTextAlign || this.centerText as CanvasTextAlign;
             ctx.fillText(item.any_string, item.x, item.y);
          }
-         if( item.type ==='image' ){
+         if( item.type === 'image' ){
            const imgData = new Image();
            imgData.src = item.any_string;
            imgData.onload = function() {
                ctx.drawImage(imgData, item.x, item.y,item.width,item.height);
            };
          }
+         if( item.type === 'qr' ){
+            /*const qr = QRCode.toDataURL('asd')
+            console.log(qr)*/
+            const imagenQr = QRCode.toFile(item.any_string, 'Some text', {
+              color: {
+                dark: '#00F',
+                light: '#0000'
+              }
+            }, function (err) {
+              if (err) throw err
+              console.log('done')
+            });
+            console.info('imagenQr',imagenQr)
+          }
        });
   }
 
---
+++ src/utils/CanvasSetters.ts
@@ -0,0 +1,26 @@
+type CanvasSetterOptions = {
+
+}
+
+
+
+export function setImage  ( canvas: HTMLCanvasElement, ops: CanvasSetterOptions) {
+    console.log(canvas)
+        return ops
+}
+
+
+export function setText  ( canvas: HTMLCanvasElement, ops: CanvasSetterOptions) {
+    console.log(canvas)
+    return ops
+}
+
+export function setQR  ( canvas: HTMLCanvasElement, ops: CanvasSetterOptions) {
+    console.log(canvas)
+    return ops
+}
+
+export function setBarcode  ( canvas: HTMLCanvasElement, ops: CanvasSetterOptions) {
+    console.log(canvas)
+    return ops
+}
